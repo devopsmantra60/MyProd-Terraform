4b32e6a5ec401a339bffcc1932dc6c5a372c1139
devopsmantra60 <devopsmantra60@gmail.com>
2026-09-12 16:26:07 +0530

    Storge code added in main.tf file

--- main.tf
+++ main.tf
@@ -2,3 +2,15 @@ resource "azurerm_resource_group" "example" {
   name     = "example"
   location = "West Europe"
 }
+
+resource "azurerm_storage_account" "example" {
+  name                     = "storage0101accountname0011"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
