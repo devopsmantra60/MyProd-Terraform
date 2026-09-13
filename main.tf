2dbd1590b92c12454aa42a3f42ecf48a726e41cd
devopsmantra60 <devopsmantra60@gmail.com>
2026-09-13 13:36:23 +0530

    changed main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "azurerm_resource_group" "example" {
   location = "West Europe"
 }
 
-resource "azurerm_resource_group" "example1" {
-  name     = "example1"
-  location = "East Europe"
+resource "azurerm_resource_group" "example2" {
+  name     = "example2"
+  location = "East Japan"
 }
